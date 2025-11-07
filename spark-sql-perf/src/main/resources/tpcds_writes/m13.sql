9abd55a750dba7eb87e203eb0b194b9ba9f68a8c
hariuserx <hchapara@uci.edu>
2025-11-07 12:22:59 -0800

    Add license headers

--- spark-sql-perf/src/main/resources/tpcds_writes/m13.sql
+++ spark-sql-perf/src/main/resources/tpcds_writes/m13.sql
@@ -1,3 +1,19 @@
+/*
+ * Copyright (2021) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 MERGE INTO web_returns_q13 t
         USING web_returns_file005_match100_notmatch001 s
         ON t.wr_order_number = s.wr_order_number AND t.wr_item_sk = s.wr_item_sk
